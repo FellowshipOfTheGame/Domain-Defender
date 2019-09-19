f01eebd4b3b9b1a64e75ad061c3686b26fbddd27
Leode Will <leode.g.will@gmail.com>
2019-09-19 08:37:34 -0300

    Hexagon Animation & Icons Imported

---
+++ Assets/Art/icons/HexagonAnimHandle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonAnimHandle : MonoBehaviour {
+
+    public Animator core, shield;
+
+
+    //back all states do start
+    public void Reset(){
+        core.SetTrigger("reset");
+        shield.SetTrigger("reset");
+    }
+
+    //active = true -> turn on shield
+    //active = false -> turno off shield
+    public void SetShield(bool active){
+        if(active)
+            shield.SetTrigger("on");
+        else
+            shield.SetTrigger("off");
+        
+    }
+
+    //final animation
+    public void GameOver(){
+        core.SetTrigger("die");
+    }
+}
