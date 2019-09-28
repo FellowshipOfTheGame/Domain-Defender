d4937f43a43d8daf3e2691ccd339fc2dcde7444e
Abner Santos <abner.santos@usp.br>
2019-09-28 00:09:39 -0300

    Added ranking menu

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -1,10 +1,12 @@
+[System.Serializable]
 public class HighScore
 {
     public int score;
     public string username;
 }
 
+[System.Serializable]
 public class HighScores
 {
-    HighScore[] highScores;
+    public HighScore[] highScores;
 }
---
+++ Assets/Scripts/HighScoreItemUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScoreItemUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI username, score, placement;
+
+    public void SetScore(HighScore highScore, int placement)
+    {
+        username.text = highScore.username;
+        score.text = highScore.score.ToString();
+        this.placement.text = placement.ToString();
+    }
+}
